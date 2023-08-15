1acec963d09a2530c0ae24029f9a6b55483026c2
Tiffany Perez <pereztb02@gmail.com>
2023-08-15 12:43:23 -0400

    completely changed js, quiz still does not work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,293 +1,101 @@
 var currentQuestion = document.querySelector(".current-question");
+var choices = document.querySelector(".choices");
 var right = document.querySelector(".correct");
 var wrong = document.querySelector(".incorrect");
 var timerElement = document.querySelector(".time-left");
 var startButton = document.querySelector(".start-button");
 
+
+//Array of questions
 let questions = [{
-   
-question: "What does HTML stand for?",
-choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
-correctAnswer: "Hyper Text Markup Language"}
 
-{question: "In Java, a method is a container that holds classes.",
-    choices: ["True", "False"],
-    correctAnswer: "False"}
+  question: "What does HTML stand for?",
+  choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+  correctAnswer: "Hyper Text Markup Language"
+},
 
+{
+  question: "In Java, a method is a container that holds classes.",
+  choices: ["True", "False"],
+  correctAnswer: "False"
+},
+{
+  question: "<h1>Text</h1 > is the correct way of making a header in HTML.",
+  choices: ["True", "False"],
+  correctAnswer: "True"
+},
+{
+  question: "Which of the following is the correct way of making a string in Java ?",
+  choices: ["String 'Text'", "String text = 'text'", "String text = 'text'", "String text = 'text'"],
+  correctAnswer: "String text = 'text'"
+},
+{
+  question: "Is this the correct way to make an object in Java ? Class class = new Class();",
+  choices: ["True", "False"],
+  correctAnswer: "True"
+},
+{
+  question: "Is this HTML code correct ? <html> <head> <title>Title</title> </head> <h1>Header</h1> <p>Paragraph</p> </html>",
+  choices: ["Yes", "No"],
+  correctAnswer: "No"
+},
+];
+//Timer count down
+const timerDuration = 100; // in seconds
+let currentQuestionIndex = 0;
+let timeLeft = timerDuration;
+
+// The startQuiz function is called when the start button is clicked
+function startQuiz() {
  
-A. 
-True
-
-B. 
-False
-
-Correct Answer(s)
-B. False
- 4. 
-<h1>Text</h1> is the correct way of making a header in HTML.
-A. 
-True
-
-B. 
-False
-
-Correct Answer(s)
-A. True
- 5. 
-Which of the following is the correct way of making a string in Java?
-A. 
-String "Text";
-
-B. 
-String text = 'text';
-
-C. 
-String text = "text"
-
-D. 
-String text = "text";
-
-Correct Answer(s)
-D. String text = "text";
- 6. 
-Which of the following is the correct way to use the standard namespace in C++?
-A. 
-Using namespace std;
-
-B. 
-Using namespace standard;
-
-C. 
-Using standard namespace;
-
-D. 
-Standard namespace used;
-
-Correct Answer(s)
-A. Using namespace std;
- 7. 
-Is this the correct way to make an object in Java? Class class = new Class();
-A. 
-True
-
-B. 
-False
-
-Correct Answer(s)
-A. True
- 8. 
-Is this how you import something in C++? #include <string>
-A. 
-No
-
-B. 
-Yes
-
-Correct Answer(s)
-B. Yes
- 9. 
-Is this HTML code correct? <html> <head> <title>Title</title> </head> <h1>Header</h1> <p>Paragraph</p> </html>
-A. 
-Yes
-
-B. 
-No
-
-Correct Answer(s)
-B. No
- 10. 
-Is it less bytes to use:       long l = 100; then:       int i = 10;
-A. 
-Yes
-
-B. 
-No
-
-Correct Answer(s)
-B. No
-}];
-
-/*var chosenWord = "";
-var numBlanks = 0;
-var rightCounter = 0;
-var wrongCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
-
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
-
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 100;
+ displayQuestion();
+ startTimer();
   // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
+  //startButton.disabled = true;
+};
+startButton.addEventListener.("click", startQuiz);
+function displayQuestion() {
+  if (currentQuestionIndex < questions.length) {
+      const question = questions[currentQuestionIndex];
+      questionElement.textContent = question.question;
+
+      choicesElement.innerHTML = "";
+      question.choices.forEach((choice, index) => {
+          const choiceButton = document.createElement("button");
+          choiceButton.textContent = choice;
+          choiceButton.addEventListener("click", () => checkAnswer(index));
+          choicesElement.appendChild(choiceButton);
+      });
+  } else {
+      endQuiz();
+  }
+};
+function checkAnswer(selectedIndex) {
+  const question = questions[currentQuestionIndex];
+  if (selectedIndex === question.correctAnswer) {
+      // Handle correct answer
+  } else {
+      // Handle wrong answer
+  }
+
+  currentQuestionIndex++;
+  displayQuestion();
 }
 
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
+  const timerInterval = setInterval(() => {
+      if (timeLeft > 0) {
+          timeLeft--;
+          timerElement.textContent = timeLeft + "s";
+      } else {
+          clearInterval(timerInterval);
+          endQuiz();
       }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
   }, 1000);
 }
 
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
+function endQuiz() {
+  // Handle quiz end, show results or feedback
 }
 
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+startQuiz();
